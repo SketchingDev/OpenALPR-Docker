314485d379e71f935c56db8914a11fe962e2d718
FlyingTopHat <FlyingTopHat@users.noreply.github.com>
2017-05-14 21:45:26 +0100

    Fix error in test script
    Fixes the error 'Conflicting options: --rm and -d'

--- test.sh
+++ test.sh
@@ -17,14 +17,14 @@ TIMEOUT=10
 
 # Test 1
 IMAGE_NAME=single_horizontal.jpg
-docker run --rm --name $CONTAINER_NAME -d $DOCKER_IMAGE_NAME http://127.0.0.1/$IMAGE_NAME --interval 1
+docker run --name $CONTAINER_NAME -d $DOCKER_IMAGE_NAME http://127.0.0.1/$IMAGE_NAME --interval 1
 
 docker exec -d $CONTAINER_NAME python -m SimpleHTTPServer 80
 docker cp ./test_files/$IMAGE_NAME $CONTAINER_NAME:/opt/docker-alpr/
 
 find_in_docker_log $CONTAINER_NAME $TIMEOUT "WR62XDF"
 TEST_RESULT=$?
-docker kill $CONTAINER_NAME
+docker rm -f $CONTAINER_NAME
 
 if [ $TEST_RESULT == 0 ]; then
     echo "SUCCESS"
@@ -36,7 +36,7 @@ fi
 
 # Test 2
 IMAGE_NAME=multiple_rotated.jpg
-docker run --rm --name $CONTAINER_NAME -d $DOCKER_IMAGE_NAME http://127.0.0.1/$IMAGE_NAME --interval 1 \
+docker run --name $CONTAINER_NAME -d $DOCKER_IMAGE_NAME http://127.0.0.1/$IMAGE_NAME --interval 1 \
     --preprocess planar,429.000000,300.000000,-0.000000,0.000000,0.670000,1.000000,1.000000,0.000000,0.000000
 
 docker exec -d $CONTAINER_NAME python -m SimpleHTTPServer 80
@@ -44,7 +44,7 @@ docker cp ./test_files/$IMAGE_NAME $CONTAINER_NAME:/opt/docker-alpr/
 
 find_in_docker_log $CONTAINER_NAME $TIMEOUT "HK57GHT"
 TEST_RESULT=$?
-docker kill $CONTAINER_NAME
+docker rm -f $CONTAINER_NAME
 
 if [ $TEST_RESULT == 0 ]; then
     echo "SUCCESS"
@@ -56,7 +56,7 @@ fi
 
 # Test 3
 IMAGE_NAME=multiple_rotated.jpg
-docker run --rm --name $CONTAINER_NAME -d $DOCKER_IMAGE_NAME http://127.0.0.1/$IMAGE_NAME --interval 1 \
+docker run --name $CONTAINER_NAME -d $DOCKER_IMAGE_NAME http://127.0.0.1/$IMAGE_NAME --interval 1 \
       --preprocess 0,0,435,299=planar,429.000000,300.000000,-0.000000,0.000000,0.670000,1.000000,1.000000,0.000000,0.000000 \
       --preprocess 435,0,705,299=planar,272.000000,300.000000,-0.000000,0.000000,-0.650000,1.000000,1.000000,0.000000,0.000000
 
@@ -65,7 +65,7 @@ docker cp ./test_files/$IMAGE_NAME $CONTAINER_NAME:/opt/docker-alpr/
 
 find_in_docker_log $CONTAINER_NAME $TIMEOUT "WR62XDF" && find_in_docker_log $CONTAINER_NAME $TIMEOUT "HK57GHT"
 TEST_RESULT=$?
-docker kill $CONTAINER_NAME
+docker rm -f $CONTAINER_NAME
 
 if [ $TEST_RESULT == 0 ]; then
     echo "SUCCESS"
